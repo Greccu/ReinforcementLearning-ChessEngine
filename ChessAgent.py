70b8265eb28ace681b7d6e459c37ea35db141b94
bogdan.grecu <bogdan.grecu@softbinator.com>
2022-01-18 15:34:19 +0200

    Added changes to console output + PGN file

--- ChessAgent.py
+++ ChessAgent.py
@@ -40,6 +40,10 @@ class ChessAgent:
         return curr_node.exploitation + self.exploration_constant * (
             sqrt(log(curr_node.parentVisits + e + (10 ** -6)) / (curr_node.visits + (10 ** -10))))
 
+
+    def load_agent(self):
+        pass
+
     # choose best child
     def selection(self, curr_node):
         children_ucbs = [self.ucb1(child) for child in curr_node.children]
@@ -102,7 +106,7 @@ class ChessAgent:
         return curr_node
 
     # mcts_pred
-    def mcts_pred(self, curr_node, over, white, iterations=1):
+    def mcts_pred(self, curr_node, over, white, iterations=20):
         if (over):
             return -1
         all_moves = [curr_node.state.san(i) for i in list(curr_node.state.legal_moves)]
@@ -138,6 +142,8 @@ class ChessAgent:
         self.env.reset()
         whites_turn = True
         moves = 0
+        pgn = []
+        f = open("game.txt","w")
         board = chess.Board()
         terminal = False
         nodes = []
@@ -149,7 +155,7 @@ class ChessAgent:
             result = self.mcts_pred(root, board.is_game_over(), whites_turn)
             root.action = str(board.parse_san(result))
 
-            print("\n" * 10)
+            print("\n" * 7)
             # poate iti trebuie pentru run_episode sa returnezi
             # nodes.append(root)
 
@@ -158,23 +164,33 @@ class ChessAgent:
             # print(board.can_claim_draw())
             
             board, reward, terminal, info = self.env.step(board.parse_san(result))
-            print("OUT")
+            # print("OUT")
             whites_turn = bool(1 - whites_turn)
             moves += 1
             print(f'Move: {(moves + 1)//2}\n\n')
             if moves/2 != moves//2:
-                print('White to move')
+                print('White to move\n')
             else:
-                print('Black to move')
+                print('Black to move\n')
+
+            print(f"Move made: {result}\n")
+            f.write(f'{(moves + 1)//2}. {result} ')
             print(self.env.render())
             
             if board.is_stalemate():
                 print("Draw by stalemate")
                 terminal = True
+            elif board.can_claim_threefold_repetition():
+                print("Draw by 3F")
+                terminal = True
+            elif board.is_insufficient_material():
+                print("Draw by insufficient material")
+                terminal = True
             elif board.can_claim_fifty_moves():
                 print('Draw by 50 Move Rule')
                 terminal = True
             if (terminal):
+                f.close()
                 return reward
 
     def bot_vs_player(self):
@@ -230,60 +246,59 @@ class ChessAgent:
             if (terminal):
                 print(reward)
 
-    def update_policy_q(self, eps=None):
-        return
-        # for state, values in self.q.items():
-        #     if eps != None: # e-Greedy policy updates ?
-        #         if np.random.rand() < eps:
-        #             self.policy[state] = self.env.action_space.sample() # sample a random action
-        #         else:
-        #             self.policy[state] = np.argmax(values)
+    # def update_policy_q(self, eps=None):
+    #     return
+    #     for state, values in self.q.items():
+    #         if eps != None: # e-Greedy policy updates ?
+    #             if np.random.rand() < eps:
+    #                 self.policy[state] = self.env.action_space.sample() # sample a random action
+    #             else:
+    #                 self.policy[state] = np.argmax(values)
 
-        #     else: # Greedy policy updates
-        #         self.policy[state] = np.argmax(values)
+    #         else: # Greedy policy updates
+    #             self.policy[state] = np.argmax(values)
 
     # train
-    def mc_control_glie(self, n_episode=500000, firstVisit=True, update_policy_every=1):
+    # def mc_control_glie(self, n_episode=500000, firstVisit=True, update_policy_every=1):
 
-        for t in trange(n_episode):
-            traversed = []
-            # Get an epsilon for this episode - used in e-greedy policy update
-            eps = self.get_epsilon(t)
+    #     for t in trange(n_episode):
+    #         traversed = []
+    #         # Get an epsilon for this episode - used in e-greedy policy update
+    #         eps = self.get_epsilon(t)
 
-        #     # Generate an episode following current policy
-        #     transitions = self.run_episode(eps=None)
+    #         # Generate an episode following current policy
+    #         transitions = self.run_episode(eps=None)
 
-        #     # zip operation will convert transitions to list of states, list of actions, rewards etc.
-        #     states, actions, rewards, next_states, dones = zip(*transitions)
+    #         # zip operation will convert transitions to list of states, list of actions, rewards etc.
+    #         states, actions, rewards, next_states, dones = zip(*transitions)
 
-        #     # If firstVisit version is used, create a table stateAction_firstVisitTime that stores
-        #     # when the pair (State, action) was seen first in this episode
-        #     if firstVisit == True:
-        #         stateAction_firstVisitTime = {}
-        #         for t, state in enumerate(states):
-        #             stateAction_t = (state, actions[t])
-        #             if stateAction_t not in stateAction_firstVisitTime:
-        #                 stateAction_firstVisitTime[stateAction_t] = t
+    #         # If firstVisit version is used, create a table stateAction_firstVisitTime that stores
+    #         # when the pair (State, action) was seen first in this episode
+    #         if firstVisit == True:
+    #             stateAction_firstVisitTime = {}
+    #             for t, state in enumerate(states):
+    #                 stateAction_t = (state, actions[t])
+    #                 if stateAction_t not in stateAction_firstVisitTime:
+    #                     stateAction_firstVisitTime[stateAction_t] = t
 
-        #     # Iterate over episode steps in reversed order, T-1, T-2, ....0
-        #     G = 0 # return output
-        #     for t in range(len(transitions)-1, -1, -1):
-        #         St = states[t]
-        #         At = actions[t]
+    #         # Iterate over episode steps in reversed order, T-1, T-2, ....0
+    #         G = 0 # return output
+    #         for t in range(len(transitions)-1, -1, -1):
+    #             St = states[t]
+    #             At = actions[t]
 
-        #         if firstVisit == True:
-        #             # Is t first time when we see the (state, action) pair ?
-        #             if stateAction_firstVisitTime[(St, At)] < t:
-        #                 continue
+    #             if firstVisit == True:
+    #                 # Is t first time when we see the (state, action) pair ?
+    #                 if stateAction_firstVisitTime[(St, At)] < t:
+    #                     continue
+    #             G = self.gamma * G + rewards[t]
 
-        #         G = self.gamma * G + rewards[t]
+    #             self.n_q[St][At] += 1
+    #             alpha = (1.0 / self.n_q[St][At]) # Remember that with this formula all episodes experiences have equal importance, if you want to forget older episode put a bigger probability
+    #             self.q[St][At] = self.q[St][At] + alpha*(G - self.q[St][At])
 
-        #         self.n_q[St][At] += 1
-        #         alpha = (1.0 / self.n_q[St][At]) # Remember that with this formula all episodes experiences have equal importance, if you want to forget older episode put a bigger probability
-        #         self.q[St][At] = self.q[St][At] + alpha*(G - self.q[St][At])
+    #         if t % update_policy_every == 0:
+    #             self.update_policy_q(eps)
 
-        #     if t % update_policy_every == 0:
-        #         self.update_policy_q(eps)
-
-        # # final policy update at the end
-        # self.update_policy_q(eps)
+    #     # final policy update at the end
+    #     self.update_policy_q(eps)
